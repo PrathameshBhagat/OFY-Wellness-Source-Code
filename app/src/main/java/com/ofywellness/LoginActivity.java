1a05679c186b2c232e737895c63b15db2f27a2f8
Prathamesh Bhagat <prathameshbhagat0608@gmail.com>
2024-09-21 16:32:41 +0530

    Trying updating the user registration UI and backend
    Signed-off-by: Prathamesh Bhagat <prathameshbhagat0608@gmail.com>

--- app/src/main/java/com/ofywellness/LoginActivity.java
+++ app/src/main/java/com/ofywellness/LoginActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -13,15 +14,23 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.ofywellness.db.ofyDatabase;
 
 public class LoginActivity extends AppCompatActivity {
     private GoogleSignInClient ofyGoogleSignInClient;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
 
         // Make the user signIn to his google account
         signInToGoogleAccount();
@@ -33,18 +42,20 @@ public class LoginActivity extends AppCompatActivity {
 
         // Objects required
         SignInButton ofyGoogleSignInButton = findViewById(R.id.google_image);
-        GoogleSignInOptions ofyGoogleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+        GoogleSignInOptions ofyGoogleSignInOptions = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.web_client_id))
+                .requestEmail().build();
+
         ofyGoogleSignInClient = GoogleSignIn.getClient(this, ofyGoogleSignInOptions);
 
         //onClick for Google Sign-In Button
-        ofyGoogleSignInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent signInIntent = ofyGoogleSignInClient.getSignInIntent();
-                // This intent will make user to signIn to his account,
-                // and  control will move onActivityResult method
-                startActivityForResult(signInIntent, 1000);
-            }
+        ofyGoogleSignInButton.setOnClickListener(view -> {
+            Intent signInIntent = ofyGoogleSignInClient.getSignInIntent();
+            // This intent will make user to signIn to his account,
+            // and  control will move onActivityResult method
+            startActivityForResult(signInIntent, 1000);
         });
     }
 
@@ -59,7 +70,19 @@ public class LoginActivity extends AppCompatActivity {
                 GoogleSignInAccount account = GoogleSignIn.getSignedInAccountFromIntent(data).getResult();
 
                 // Show a Toast message
-                Toast.makeText(getApplicationContext(), "Google account: " + account.getDisplayName(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Google account: " + account.getIdToken() + account.getDisplayName(), Toast.LENGTH_SHORT).show();
+                AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
+                firebaseAuth.signInWithCredential(firebaseCredential).addOnCompleteListener(this, task -> {
+                    // Check condition
+                    if (task.isSuccessful()) {
+                        // When task is successful redirect to profile activity display Toast
+                        startActivity(new Intent(MainActivity.this, ProfileActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                        displayToast("Firebase authentication successful");
+                    } else {
+                        // When task is unsuccessful display Toast
+                        displayToast("Authentication Failed :" + task.getException().getMessage());
+                    }
+                });
 
                 // Check if the user is present in Firebase Database with email
                 // And if found move to next activity
